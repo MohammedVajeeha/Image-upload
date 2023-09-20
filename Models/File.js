764730e8e66dc398d7fe568d0540b627e9e1cd16
MohammedVajeeha <86507560+MohammedVajeeha@users.noreply.github.com>
2023-09-20 06:43:03 +0530

    Delete Models directory

--- Models/File.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const fileSchema = new mongoose.Schema({
-  filename: String,
-  path: String,
-  type: String, // 'image' or 'video'
-});
-
-module.exports = mongoose.model('File', fileSchema);
